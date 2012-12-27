760a36498442face30efa4646f6d6bb95f3e7e7c
Stefan Nagey <stefan@nagey.com>
2012-12-26 22:34:24 -0500

    added initial for table script

---
+++ table.rb
@@ -0,0 +1,11 @@
+dep 'table.src' do
+  require 'gcc', 'rabbitmq'
+
+  source "git@github.com:/mirrordoor/md-table"
+ 
+  provides 'table >= 0.1.0'
+
+  configure_env "LIBBFD='-lbfd'" if host.linux?
+  configure_env "STD='-std=c++0x'" if host.linux?
+  configure_env "WERROR='-Werror'" if host.linux?
+end
