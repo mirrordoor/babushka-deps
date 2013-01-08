bfa94b8e7c218af391ec539f262da368d640437b
Stefan Nagey <stefan@nagey.com>
2013-01-07 22:15:04 -0500

    added base of rabbitmq - still needs to check that /usr/local/sbin is in the path

---
+++ rabbitmq.rb
@@ -0,0 +1,25 @@
+# Shamelessly stolen from github.com/ivanvanderbyl & added mac support
+
+dep('rabbitmq-server.managed') {
+  requires {
+    on :ubuntu, 'updated rabbitmq source'
+  }
+  
+  installs {
+    via :apt, 'rabbitmq-server' if Babushka::host.linux?
+    via :brew, "rabbitmq" if Babushka::host.osx?
+  }
+  
+  provides %w(rabbitmq-server rabbitmqctl)  
+}
+
+dep('updated rabbitmq source') {
+  met? { File.exists? "/etc/apt/sources.list.d/rabbitmq.list"}
+  meet {
+    shell 'echo "deb http://www.rabbitmq.com/debian/ testing main" | sudo tee /etc/apt/sources.list.d/rabbitmq.list'
+    shell 'wget -qO - http://www.rabbitmq.com/rabbitmq-signing-key-public.asc | sudo apt-key add -'
+    shell 'sudo apt-get update'
+  }
+}
+
+dep "rabbitmq-c.managed"
