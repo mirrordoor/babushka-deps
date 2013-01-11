fa2b53af37564d976f952eca7698e9b06c586073
Stefan Nagey <stefan@nagey.com>
2013-01-10 22:21:12 -0500

    made script start use envdir and set rabbit to localhost

--- hhLogger.rb
+++ hhLogger.rb
@@ -4,6 +4,7 @@ dep 'md-handHistory-logger' do
     "nodejs.bin", 
     "mongodb.managed", 
     "md-package up to date".with(:package => package), 
+    "md-package setenv".with(:package => package, :key => "RABBIT_HOST", :value => "localhost"),
     "md-package running".with(:package => package)
   ]
   met? { true }
@@ -14,6 +15,7 @@ def md_handHistory_logger_run_script_contents
 #!/bin/sh
 npm install
 npm start
+envdir env npm start
   rsc
 end
 
