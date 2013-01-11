3bf7fa5569ea9b1716e6a5eb056202d9c11d6b88
Stefan Nagey <stefan@nagey.com>
2013-01-10 23:58:23 -0500

    dep to make sure bower is installed

--- md-client-html5.rb
+++ md-client-html5.rb
@@ -14,6 +14,12 @@ dep 'md-client-html5' do
     "nagey:running.nginx".with(:nginx_prefix => "/usr/local/nginx")
   ]
 end
+dep "bower installed" do
+  requires "npm"
+  met? { File.exists? md_web_dir('client-html5')+"/node_modules/bower/" }
+  meet { shell "cd #{md_web_dir('client-html5')}; npm install bower" }
+end
+
 dep "hostname configured", :myhostname do
   met? { shell?("cat /etc/hosts|grep #{myhostname}") || shell?("host #{myhostname}") }
   meet { sudo "echo '127.0.0.1 #{myhostname}' >> /etc/hosts" }
