2f5ca2157b032a9e80d673b0d0fc79d82f3825b1
Stefan Nagey <stefan@nagey.com>
2013-01-10 22:22:02 -0500

    use envdir and set port of 3002

--- hhViewer.rb
+++ hhViewer.rb
@@ -4,6 +4,7 @@ dep 'md-handHistory-viewer' do
     "nodejs.bin", 
     "mongodb.managed", 
     "md-package up to date".with(:package => package), 
+    "md-package setenv".with(:package => package, :key => "PORT", :value => "3002"),
     "md-package running".with(:package => package)
   ]
 end
@@ -12,7 +13,7 @@ def md_handHistory_viewer_run_script_contents
   <<-rsc
 #!/bin/sh
 npm install
-npm start
+envdir env npm start
   rsc
 end
 
