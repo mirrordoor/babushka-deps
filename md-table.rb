c284ae168a7d488e0c8b273d5f55d32131524c24
Stefan Nagey <stefan@nagey.com>
2013-03-03 22:41:31 -0500

    more for table

--- md-table.rb
+++ md-table.rb
@@ -1,7 +1,7 @@
 host = Babushka::host
 
 dep 'table.src' do
-  requires 'gcc', 'binutils.managed', 'rabbitmq-c.managed', 'nagey:coreutils.managed', "md-package cloned".with(:package => "md-table")
+  requires 'gcc', 'binutils.managed', 'rabbitmq-c.managed', 'libjsoncpp0.managed', 'libjsoncpp-dev.managed', 'librabbitmq-dev.managed', 'librabbitmq0.managed', 'nagey:coreutils.managed', "md-package cloned".with(:package => "md-table")
 
   source File.expand_path("~/src")
  
@@ -30,5 +30,24 @@ dep "md-table" do
 end
 
 dep "md-table built" do
-  requires "rabbitmq-c.managed", "jsoncpp"
+  requires 'gcc', 'binutils.managed', 'rabbitmq-c.managed', 'libjsoncpp0.managed', 'libjsoncpp-dev.managed', 'librabbitmq-dev.managed', 'librabbitmq0.managed', 'nagey:coreutils.managed'
+  provides 'table >= 0.1.0'
+  configure_env "LIBBFD='-lbfd'" if host.linux?
+  configure_env "STD='-std=c++0x'" if host.linux?
+  configure_env "WERROR='-Werror'" if host.linux?
+
+  configure_env "LIBBFD=" if host.osx?
+  configure_env "STD=" if host.osx?
+  configure_env "WERROR=" if host.osx?
+  
+  something = false
+  
+  met? { something }
+  meet do
+    log_shell "clean", "make clean"
+    log_shell "build", "make"
+    Babushka::SrcHelper.install_src! "make install"
+    something = true
+  end
+  
 end
