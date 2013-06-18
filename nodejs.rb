191676e511a8394f5eeabe7d142d92fbb41951fa
Stefan Nagey <stefan@nagey.com>
2013-06-18 13:22:34 -0400

    make sure /usr/local/nodejs is in the path

--- nodejs.rb
+++ nodejs.rb
@@ -1,7 +1,16 @@
+:nodepath = "/usr/local/nodejs"
+
 dep 'nodejs.src', :version do
+  requires "node_path"
   version.default!('0.10.11')
   source "http://nodejs.org/dist/v#{version}/node-v#{version}.tar.gz"
   provides "node ~> #{version}"
-  prefix "/usr/local/nodejs"
+  prefix :nodepath
   met? { in_path? "node >= #{version}" }
 end
+
+dep "node_path" do
+  met? { shell? "echo $PATH | grep #{:nodepath}" }
+  meet { sudo "echo #{:nodepath} >> /etc/profile" }
+end
+  
