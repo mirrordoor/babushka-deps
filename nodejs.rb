16992bd0a6af7ac3ae1be2502f2015a5a00126a5
Stefan Nagey <stefan@nagey.com>
2013-06-18 13:26:48 -0400

    syntax error in previous commit

--- nodejs.rb
+++ nodejs.rb
@@ -1,15 +1,15 @@
-:nodepath = "/usr/local/nodejs"
-
 dep 'nodejs.src', :version do
-  requires "node_path"
+  nodepath = "/usr/local/nodejs"
+  
+  requires "node_path", with :nodepath => nodepath
   version.default!('0.10.11')
   source "http://nodejs.org/dist/v#{version}/node-v#{version}.tar.gz"
   provides "node ~> #{version}"
-  prefix :nodepath
+  prefix nodepath
   met? { in_path? "node >= #{version}" }
 end
 
-dep "node_path" do
+dep "node_path", :nodepath do
   met? { shell? "echo $PATH | grep #{:nodepath}" }
   meet { sudo "echo #{:nodepath} >> /etc/profile" }
 end
